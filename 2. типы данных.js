15f03d0186212cc78c75c99fdad9f9fc9f954575
Rahman <rahmangadzh1994@gmail.com>
2020-11-18 21:25:18 +0300

    update

---
+++ 2. типы данных.js
@@ -0,0 +1,20 @@
+// строки
+let string = 'test';
+let string2 = "test"; // разницы нет
+let string3 = new String('test'); // создается обсъект строки
+
+// у строки бывают методы(помощники
+string.charAt(0) // выведет первую букву t
+// ссылка на все методы https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String#Methods
+
+
+
+// числа
+let number = 25;
+let number2 = new Number(25);
+
+// у чисел есть методы
+number.toFixed(2) // 25.00
+// ссылка на все методы https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number#Methods
+
+
